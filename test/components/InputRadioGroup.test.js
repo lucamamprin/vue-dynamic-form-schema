e893e6aab9f276bb205a1c2682c31c9ed16685fc
Luca Mamprin <lucamamprin@users.noreply.github.com>
2021-02-15 23:28:08 +0100

    Test input radio group (#42)
    * added test for InputRadioGroup
    
    * add test for options validations
    
    * add test for error message; fix required
    
    * Revert "add test for error message; fix required"
    
    * add disabled and error test, fix errormessage prop

---
+++ test/components/InputRadioGroup.test.js
@@ -0,0 +1,77 @@
+import Vue from "vue"
+import { shallowMount } from "@vue/test-utils"
+import InputRadioGroup from "../../src/components/FormElements/InputRadioGroup.vue"
+
+const props = {
+  name: "hobby",
+  id: "user-hobby",
+  label: "What is your hobby?",
+  describedBy: "hobby-describe",
+  options: [
+    {
+      "label": "Cinema",
+      "value": "cinema",
+    },
+    {
+      "label": "Bird watching",
+      "value": "bird_watching",
+    },
+    {
+      "label": "Coding",
+      "value": "coding",
+    },
+  ],
+}
+
+let wrapper
+
+describe("InputRadioGroup", () => {
+  beforeEach(() => {
+    wrapper = shallowMount(InputRadioGroup, {
+      propsData: props,
+    })
+  })
+  
+  test("mounts correctly", () => {
+    expect(wrapper.vm).toBeTruthy()
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  test("can be disabled", async () => {
+    await wrapper.setProps({
+      disabled: true,
+    })
+
+    await Vue.nextTick()
+    
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+  
+  test("validates options", () => {
+    const validator = InputRadioGroup.props.options.validator
+    expect(validator(props.options)).toBe(true)
+  })
+
+  test("validates wrong options", () => {
+    const wrongOptions =  [
+      {
+        "wrong-label": "Cinema",
+        "wrong-value": "cinema",
+      },
+    ]
+    
+    const validator = InputRadioGroup.props.options.validator
+    expect(validator(wrongOptions)).toBe(false)
+  })
+  
+  test("shows error message", async () => {
+    await wrapper.setProps({
+      hasError: true,
+      errorMessage: "You didn't select any hobby",
+    })
+
+    await Vue.nextTick()
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
